f11fc6282cc154984a466fc337ad1919821f82dd
VeselAbaya <Vandervise465@gmail.com>
2019-07-13 12:46:40 +0300

    category model: oneTimeLimit & periodLimit

--- server/db/models/record/category.js
+++ server/db/models/record/category.js
@@ -7,13 +7,25 @@ const Category = mongoose.model('Category', {
     type: String,
     required: true
   },
-  limit: {
+  oneTimeLimit: {
     type: Number,
     validate: {
-      validator: (limit) => limit > 0,
+      validator: (limit) => limit > 0 || limit === null,
       message: '{VALUE} must be bigger than 0'
     },
     default: Infinity
+  },
+  periodLimit: {
+    type: Number,
+    validate: {
+      validator: (limit) => limit > 0 || limit === null,
+      message: '{VALUE} must be bigger than 0'
+    },
+    default: Infinity
+  },
+  currentCosts: {
+    type: Number,
+    default: 0
   },
   _userId: {
     type: mongoose.Schema.Types.ObjectId,
